0bc5f5f7cf398ff63370b81e27ebbbf390a5d43f
melissapadilhacoleman365 <79901478+melissapadilhacoleman365@users.noreply.github.com>
2021-06-30 16:34:39 +0200

    Update and rename Sprint 2 Capstone Projekt.py to Sprint 1 Capstone Projekt.py

--- Sprint 1 Capstone Projekt.py
+++ Sprint 1 Capstone Projekt.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[13]:
-
+#This notebook will be mainly used for the capstone project.
+#These programs will be necessary to analyze data 
 
 import pandas as pd
 import requests
@@ -11,20 +9,19 @@ print("Imported!")
 
 
 # In[14]:
-
+#Within the project, these data sources will be used and scrapped using the following programs 
 
 url = "https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M"
 extracting_data = requests.get(url).text
 wiki_data = BeautifulSoup(extracting_data, 'lxml')
 
 
-# In[15]:
-
+# In[15]: the wikipedia data will be then organized into a graph based on the following parameters
 
 wiki_data
 
 
-# In[16]:
+# In[16]: the data will be organized in the following manner
 
 
 column_names = ['Postalcode','Borough','Neighborhood']
@@ -50,10 +47,9 @@ for tr in table.find_all('tr'):
     toronto = toronto.append({'Postalcode': postcode,'Borough': borough,'Neighborhood': neighborhood},ignore_index=True)
 
 
-# In[17]:
+# In[17]: the data will be cleaned in the following manner 
 
-
-# clean dataframe 
+clean dataframe 
 toronto = toronto[toronto.Borough!='Not assigned']
 toronto = toronto[toronto.Borough!= 0]
 toronto.reset_index(drop = True, inplace = True)
@@ -64,14 +60,14 @@ for i in range(0,toronto.shape[0]):
         i = i+1
 
 
-# In[18]:
+# In[18]:the following data fram will be created 
 
 
 df = toronto.groupby(['Postalcode','Borough'])['Neighborhood'].apply(', '.join).reset_index()
 df
 
 
-# In[19]:
+# In[19]:the data frame will be described in the following manner 
 
 
 df.describe()
@@ -85,7 +81,7 @@ empty = 'Not assigned'
 df = df[(df.Postalcode != empty ) & (df.Borough != empty) & (df.Neighborhood != empty)]
 
 
-# In[21]:
+# In[21]: the data will be grouped as follows 
 
 
 def neighborhood_list(grouped):    
@@ -95,20 +91,20 @@ grp = df.groupby(['Postalcode', 'Borough'])
 df_2 = grp.apply(neighborhood_list).reset_index(name='Neighborhood')
 
 
-# In[22]:
+# In[22]:the data will be viewed as followed 
 
 
 df_2.describe()
 
 
-# In[23]:
+# In[23]:the data will be visalized as follows
 
 
 print(df_2.shape)
 df_2.head()
 
 
-# In[24]:
+# In[24]:the data will be saved as follows
 
 
 df_2.to_csv('toronto.csv', index=False)
